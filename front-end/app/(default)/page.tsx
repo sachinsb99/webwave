19242956fc9d895a7dce88f57defe310202c346b
Klinton A <klinton.developer365@gmail.com>
2025-07-22 23:23:59 +0530

    map configured

--- front-end/app/(default)/page.tsx
+++ front-end/app/(default)/page.tsx
@@ -1,6 +1,6 @@
 export const metadata = {
-  title: "Home - Open PRO",
-  description: "Page description",
+  title: "Bug Busters",
+  description: "Let's trash your Bugs",
 };
 
 import PageIllustration from "@/components/page-illustration";
@@ -9,16 +9,18 @@ import Workflows from "@/components/workflows";
 import Features from "@/components/features";
 import Testimonials from "@/components/testimonials";
 import Cta from "@/components/cta";
+import { WorldMapDemo } from "@/components/world-map";
 
 export default function Home() {
   return (
     <>
-      <PageIllustration />
+      {/* <PageIllustration /> */}
+      {/* <WorldMapDemo /> */}
       <Hero />
       <Workflows />
       <Features />
       <Testimonials />
-      <Cta />
+      {/* <Cta /> */}
     </>
   );
 }
