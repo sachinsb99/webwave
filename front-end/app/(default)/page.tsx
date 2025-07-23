176f9c213cc5e1ff03fe373aa070085a217339d6
Klinton A <klinton.developer365@gmail.com>
2025-07-23 23:16:59 +0530

    globe added

--- front-end/app/(default)/page.tsx
+++ front-end/app/(default)/page.tsx
@@ -13,6 +13,8 @@ import { WorldMapDemo } from "@/components/world-map";
 import { FeaturesSectionDemo1 } from "@/components/features1";
 import { FeaturesSectionDemo } from "@/components/feature2";
 import { FollowingPointerDemo } from "@/components/following-pointer";
+import { CometCardDemo } from "@/components/comet-card-demo";
+import { GlobeDemo } from "@/components/globe-demo";
 
 export default function Home() {
   return (
@@ -20,9 +22,11 @@ export default function Home() {
       {/* <PageIllustration /> */}
       {/* <WorldMapDemo /> */}
       <Hero />
-      <FeaturesSectionDemo1 />
       <FeaturesSectionDemo />
-      <FollowingPointerDemo />
+      <FeaturesSectionDemo1 />
+      <GlobeDemo />
+      {/* <CometCardDemo /> */}
+      {/* <FollowingPointerDemo /> */}
       <Workflows />
       <Features />
       <Testimonials />
--- front-end/components/features1.tsx
+++ front-end/components/features1.tsx
@@ -7,6 +7,7 @@ import { useEffect, useRef } from "react";
 import { motion } from "motion/react";
 import { IconBrandYoutubeFilled } from "@tabler/icons-react";
 import { FollowingPointerDemo } from "@/components/following-pointer";
+import { CometCardDemo } from "@/components/comet-card-demo";
 
 
 export function FeaturesSectionDemo1() {
@@ -109,21 +110,15 @@ const FeatureDescription = ({ children }: { children?: React.ReactNode }) => {
 export const SkeletonOne = () => {
   return (
     <div className="relative flex py-8 px-2 gap-10 h-full">
-      <div className="w-full  p-5  mx-auto bg-white dark:bg-neutral-900 shadow-2xl group h-full">
-        <div className="flex flex-1 w-full h-full flex-col space-y-2  ">
+      {/* <div className="w-full  p-5  mx-auto bg-white dark:bg-neutral-900 shadow-2xl group h-full">
+        <div className="flex flex-1 w-full h-full flex-col space-y-2  "> */}
           {/* TODO */}
-          <img
-            src="/linear.webp"
-            alt="header"
-            width={800}
-            height={800}
-            className="h-full w-full aspect-square object-cover object-left-top rounded-sm"
-          />
-        </div>
-      </div>
+          <CometCardDemo />
+        {/* </div>
+      </div> */}
 
-      <div className="absolute bottom-0 z-40 inset-x-0 h-60 bg-gradient-to-t from-white dark:from-black via-white dark:via-black to-transparent w-full pointer-events-none" />
-      <div className="absolute top-0 z-40 inset-x-0 h-60 bg-gradient-to-b from-white dark:from-black via-transparent to-transparent w-full pointer-events-none" />
+      {/* <div className="absolute bottom-0 z-40 inset-x-0 h-60 bg-gradient-to-t from-white dark:from-black via-white dark:via-black to-transparent w-full pointer-events-none" />
+      <div className="absolute top-0 z-40 inset-x-0 h-60 bg-gradient-to-b from-white dark:from-black via-transparent to-transparent w-full pointer-events-none" /> */}
     </div>
   );
 };
