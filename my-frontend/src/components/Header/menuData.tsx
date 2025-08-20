07742bfea21f351eab973f3ee4758fe96f9fdc3a
Klinton A <klinton.developer365@gmail.com>
2025-08-20 22:56:28 +0530

    header issue fixed

---
+++ my-frontend/src/components/Header/menuData.tsx
@@ -0,0 +1,29 @@
+import { Menu } from "@/types/menu";
+
+const menuData: Menu[] = [
+  {
+    id: 1,
+    title: "Home",
+    path: "/home",
+    newTab: false,
+  },
+  {
+    id: 2,
+    title: "About",
+    path: "/main/about",
+    newTab: false,
+  },
+  {
+    id: 33,
+    title: "Blog",
+    path: "/main/blog",
+    newTab: false,
+  },
+  {
+    id: 3,
+    title: "Support",
+    path: "/main/contact",
+    newTab: false,
+  },
+];
+export default menuData;
